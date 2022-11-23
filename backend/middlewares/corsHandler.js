4d8eb815a2bdaf692750e3f6cfbe44f50e23f1ae
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-11-24 02:14:55 +0300

    Update corsHandler.js

--- backend/middlewares/corsHandler.js
+++ backend/middlewares/corsHandler.js
@@ -1,35 +1,32 @@
-// Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'http://one-for-study.nomoredomains.icu/',
-  'https://one-for-study.nomoredomains.icu/',
-  'http://api.one-for-study.nomoredomains.icu/',
-  'https://api.one-for-study.nomoredomains.icu/',
-  'localhost:3000',
-  'localhost:3001',
+  'https://one-for-study.nomoredomains.icu',
+  'http://one-for-study.nomoredomains.icu',
+  //'https://one-for-study.nomoredomains.icu/',
+  //'http://one-for-study.nomoredomains.icu/',
+  //'http://api.one-for-study.nomoredomains.icu/',
+  //'https://api.one-for-study.nomoredomains.icu/',
+  //'http://localhost:3000',
+  'http://localhost:3001',
+  //'https://localhost:3000',
+  //'https://localhost:3001',
+  //'http://localhost',
+  //'https://api.one-for-study.nomoredomains.icu/signup',
+  //'https://one-for-study.nomoredomains.icu',
 ];
 
-// eslint-disable-next-line consistent-return
-module.exports = ((err, req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
+module.exports.cors = (req, res, next) => {
+  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  // Если это предварительный запрос, добавляем нужные заголовки
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header(
-      'Access-Control-Allow-Methods',
-      DEFAULT_ALLOWED_METHODS,
-      'Access-Control-Allow-Headers',
-      requestHeaders,
-    );
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-
-  next();
-});
+  return next();
+};
