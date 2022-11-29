d1d66c5a2af8275c2d522c118864445205e9fc25
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-11-29 23:26:41 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    next(new AuthError('Что-то пошло не так. Пожалуйста, попробуйте еще раз.'));
+    return next(new AuthError('Что-то пошло не так. Пожалуйста, попробуйте еще раз.'));
   }
   let payload;
   try {
